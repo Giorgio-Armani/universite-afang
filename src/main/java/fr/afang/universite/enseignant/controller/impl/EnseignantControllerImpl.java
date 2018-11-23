78310117497e7c8c21b63baeed036b2448881571
59013-64-03 <59013-64-03@exchange.ad.afpanet>
2018-11-23 10:55:06 +0100

    sonar correction

--- src/main/java/fr/afang/universite/enseignant/controller/impl/EnseignantControllerImpl.java
+++ src/main/java/fr/afang/universite/enseignant/controller/impl/EnseignantControllerImpl.java
@@ -17,7 +17,7 @@ import fr.afang.universite.enseignant.domain.Enseignant;
 @Controller
 public class EnseignantControllerImpl implements IEnseignantController {
 
-	private final static String ENSEIGNANT = "enseignant";
+	private final String ENSEIGNANT = "enseignant";
 	@Autowired
 	private IEnseignantBusiness enseignantBusiness;
 
